5fe6c7c2cc621986ffc34db580002d76d4a5cc48
BK <kbomin328@gmail.com>
2024-05-31 12:41:17 +0900

    BookManager 수정

--- mission1/src/mission1/BookManager.java
+++ mission1/src/mission1/BookManager.java
@@ -12,7 +12,7 @@ public class BookManager {
 	}
 	
 	// 새로운 Comparator 정의
-	public static Comparator<Book> id_comp = new Comparator<Book>() {
+	public static Comparator<Book> idComparator = new Comparator<Book>() {
 		@Override
 		public int compare(Book b1, Book b2) {
 			return (b1.getId() > b2.getId() ) ? 1 : (b1.getId() < b2.getId()) ? -1 : 0 ;
@@ -20,19 +20,21 @@ public class BookManager {
 	};
 	
 	// 이진탐색 메소드
-	public void search_bs(Integer srcid) {
+	public boolean search_bs(Integer srcId) {
 	
 		// id 순으로 books 정렬
-		Collections.sort(books, id_comp);
+		Collections.sort(books, idComparator);
 		
-		// srcid 기준으로 검색 후 idx 반환
-		int search_result = Collections.binarySearch(books, new Book(srcid), id_comp);
+		// srcId 기준으로 검색 후 idx 반환
+		int search_result = Collections.binarySearch(books, new Book(srcId), idComparator);
 		
-		if (search_result<0){
+		if (search_result < 0){
             System.out.println("검색된 도서가 없습니다.");
+            return false;
 		}
 		else {
 			System.out.println("검색 결과: " + this.books.get(search_result));
+			return true;
 		}
 	}
 	
@@ -40,39 +42,36 @@ public class BookManager {
 	// 도서 추가
 	public void addBook(Integer id, String title, String author, int year) {
 		for(Book b: books) {
-			if(id==b.getId()) {
+			if(id == b.getId()) {
 				System.out.println("해당 ID("+id+") 는 이미 존재합니다.");
+				return;
 			}
 		}
-		
 		Book book = new Book(id, title, author, year);
 		books.add(book);
-		System.out.println("Book{id: '"+id+"', 제목: '"+title+"', 저자: '"+author+"', 출판년도: '"+year+"}도서가 추가되었습니다.");
-	
-		
+		System.out.println("Book{id: '"+id+"', 제목: '"+title+"', 저자: '"+author+"', 출판연도: "+year+"}도서가 추가되었습니다.");
 		return;
 	}
 	
-	// 도서 검색 -> 불필요하지 않나요??
-	public int searchBook() {
-		System.out.println("검색 결과:");
-		if(books.size()==0) {
-			System.out.println("검색된 도서가 없습니다.");
-			return 0;
-		}
-		
+	// 도서 검색 -> 이진탐색과 성능 비교 필요
+	public boolean searchBook(Integer id) {		
 		for(Book b:books) {
-			System.out.println("Book{id: '"+b.getId()+"', 제목: '"+b.getTitle()+"', 저자: '"+b.getAuthor()+"', 출판년도: '"+b.getYear()+"}도서가 추가되었습니다.");
+			if(b.getId() == id) {
+				System.out.println("검색 결과:");
+				System.out.println("Book{id: '"+b.getId()+"', 제목: '"+b.getTitle()+"', 저자: '"+b.getAuthor()+"', 출판연도: "+b.getYear()+"}");
+				return true;
+			}
 		}
-		return books.size();
+		System.out.println("검색된 도서가 없습니다.");
+		return false;
 	}
 
 	// 도서 삭제 
 	public void removeBook(Integer id) {
 		for(Book b: books) {
-			if(b.getId()==id) {
-				books.remove((int) id);
-				System.out.println("Book{id: '"+b.getId()+"', 제목: '"+b.getTitle()+"', 저자: '"+b.getAuthor()+"', 출판년도: '"+b.getYear()+"}도서를 삭제했습니다.");
+			if(b.getId() == id) {
+				books.remove(b);
+				System.out.println("Book{id: '"+b.getId()+"', 제목: '"+b.getTitle()+"', 저자: '"+b.getAuthor()+"', 출판연도: "+b.getYear()+"}도서를 삭제했습니다.");
 				return;
 			}
 			
